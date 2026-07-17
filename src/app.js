d42c930a5268ede770b302a98f99b6305aaca1d8
omorsultan <omorsultansust@gmail.com>
2026-07-17 08:29:48 +0600

    app and server

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 
+const express = require("express");
+const app = express();
+
+
+
+
 const userRoutes = require('./routes/user.routes');
 const productRoutes = require('./routes/product.routes');
 const logger = require("./middleware/logger");
-const express = require("express");
-
-const app = express();
 
 
 app.use("/users", userRoutes);
@@ -15,16 +18,8 @@ app.get("/", (req, res) => {
     res.status(201).send("Home Page");
 });
 
-app.get("/student", (req, res) => {
-    res.json({
-        name: "Omor",
-        department: "Software Engineering"
-    });
-});
 
-app.listen(3200, () => {
-    console.log("Server is running on http://localhost:3200");
-});
 
+module.exports = app;
 
 

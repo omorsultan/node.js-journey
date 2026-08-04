c397feb542307de06b070a7668ba474e05fb04ae
omorsultan <omorsultansust@gmail.com>
2026-08-04 14:17:43 +0600

    all sorting in product . vvi assume code

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,11 +1,30 @@
 const pool = require("../database/db");
 
-exports.getProducts = async () => {
+exports.getProducts = async ({ stock, sortBy, order, limit }) => {
 
-    const rows = await pool.query(
-        "SELECT * FROM products"
-    );
-    console.log(rows[0]);
+    let sql = "SELECT id, name, price, stock FROM products";
+    const values = [];
 
-    return rows[0];
+    // WHERE
+    if (stock) {
+        sql += " WHERE stock > ?";
+        values.push(Number(stock));
+    }
+
+    // ORDER BY
+    const allowedColumns = ["id", "name", "price", "stock"];
+    const column = allowedColumns.includes(sortBy) ? sortBy : "id";
+    const direction = order === "DESC" ? "DESC" : "ASC";
+
+    sql += ` ORDER BY ${column} ${direction}`;
+
+    // LIMIT
+    if (limit) {
+        sql += " LIMIT ?";
+        values.push(Number(limit));
+    }
+
+    const [rows] = await pool.query(sql, values);
+
+    return rows;
 };
